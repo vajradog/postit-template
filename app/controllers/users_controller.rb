ee2431f31b66b26ed40011eddff0e037f4f535fd
vajradog <vajradog@gmail.com>
2014-07-03 20:09:33 -0400

    Welcome greeting includes username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController <ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice]= "Welcome, your account was created and you are now logged in"
+      flash[:notice]= "Welcome, #{@user.username} your account was created and you are now logged in"
       session[:user_id] = @user.id
       redirect_to root_path
     else
