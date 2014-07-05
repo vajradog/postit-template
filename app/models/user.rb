e943f8b3d699e6403010bbfb67fc705b881885a2
vajradog <vajradog@gmail.com>
2014-07-05 08:28:30 -0400

    username downcased before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User <ActiveRecord::Base
+  before_save { self.username = username.downcase }
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes
 
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true, length:{minimum: 5}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length:{minimum: 5}
   validates :password, presence: true, length:{minimum: 5}, on: :create
+
 end
