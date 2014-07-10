f3183d4daf48ce868fab89e4d3350a4a3a0e9bcd
vajradog <vajradog@gmail.com>
2014-07-09 21:56:18 -0400

    more robust regex for slugging

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,6 +11,30 @@ class Category < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.name.gsub(/\p{^Alnum}/, '-').downcase
+    the_slug = to_slug(self.name)
+    category = Category.find_by slug: the_slug
+    count = 2
+    while category && category != self
+      the_slug = append_suffix(the_slug, count)
+      category = Category.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
   end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i !=0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/,"-"
+    str
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,30 @@ class User < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.username.gsub(/\p{^Alnum}/, '-').downcase
+    the_slug = to_slug(self.username)
+    post = User.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
   end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i !=0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/,"-"
+    str
+  end
+
 end
