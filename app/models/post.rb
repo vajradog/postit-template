a943311ec484637e63c6bc22e1de91c994010bc1
vajradog <vajradog@gmail.com>
2014-07-10 08:16:10 -0400

    extracted the sluggable method to a module and used it on Category, user and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,43 +1,12 @@
 class Post < ActiveRecord::Base
   include Voteable
+  include Sluggable
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments, dependent: :destroy, order: 'created_at DESC'
   has_many :post_categories
   has_many :categories, through: :post_categories
 
   validates :title, presence: true
-  before_save :generate_slug
-  # before_create will generate slug once only. 
-  # helpful if we don't want the slug to change on update, etc.
 
-  def to_param
-    self.slug
-  end
-
-  def generate_slug
-    the_slug = to_slug(self.username)
-    post = Post.find_by slug: the_slug
-    count = 2
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = Post.find_by slug: the_slug
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i !=0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/,"-"
-    str
-  end
+  sluggable_column :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include Sluggable
   before_save { self.username = username.downcase }
 
   has_many :posts, dependent: :destroy
@@ -11,37 +12,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 5 },
             on: :create
 
-  before_save :generate_slug
+  sluggable_column :username
 
-  def to_param
-    self.slug
-  end
-
-  def generate_slug
-    the_slug = to_slug(self.username)
-    post = User.find_by slug: the_slug
-    count = 2
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = User.find_by slug: the_slug
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i !=0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/,"-"
-    str
-  end
 
 end
